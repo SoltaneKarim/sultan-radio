2c9d41c7dbacbfe0d9dff1deb8efde1247e91c74
SoltaneKarim <karimsoltane92@gmail.com>
2024-04-28 15:50:47 +0100

    added a ? before .replace

--- src/components/CardDetails.jsx
+++ src/components/CardDetails.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const CardDetails = ({ name }) => {
-  const song = (name.split("/")[5]).replace(/%20/g, " ").replace(/.mp3/g, "").replace(/%5/g, "").replace(/%80/g, "").replace(/with lyrics/g, "").replace(/.(Official Video)/g, "").replace(/%E2/g, "").replace(/.(Official Video)/g, "").replace(/%99/g, "").replace(/%C3/g, "").replace(/%A9/g, "")
+  const song = (name.split("/")[5])?.replace(/%20/g, " ")?.replace(/.mp3/g, "")?.replace(/%5/g, "")?.replace(/%80/g, "")?.replace(/with lyrics/g, "")?.replace(/.(Official Video)/g, "")?.replace(/%E2/g, "")?.replace(/.(Official Video)/g, "")?.replace(/%99/g, "")?.replace(/%C3/g, "")?.replace(/%A9/g, "")
   console.log(name)
   return (
     <div className="card">
