6568abbb489f5f85b4500137b77a0c17217654f6
SoltaneKarim <karimsoltane92@gmail.com>
2024-04-28 18:58:27 +0100

    test2

--- src/components/CardDetails.jsx
+++ src/components/CardDetails.jsx
@@ -1,22 +1,13 @@
 import React from 'react';
 
+const toRemove = ["%20", ".mp3", "%5", "%80", "with","With","lyrics","Lyrics",".(Official Video)", "%E2", "Official", "official", "%99", "%C3", "%F0", "%9F", "%8E", "%A7","v1","version","MusicVideo","Video",""];
+
 const cleanSongName = (name) => {
-  return name
-    ?.split("/")[5]
-    ?.replace(/%20/g, " ")
-    ?.replace(/.mp3/g, "")
-    ?.replace(/%5/g, "")
-    ?.replace(/%80/g, "")
-    ?.replace(/with lyrics/g, "")
-    ?.replace(/.(Official Video)/g, "")
-    ?.replace(/%E2/g, "")
-    ?.replace(/.(Official Video)/g, "")
-    ?.replace(/%99/g, "")
-    ?.replace(/%C3/g, "")
-    ?.replace(/%F0/g, "")
-    ?.replace(/%9F/g, "")
-    ?.replace(/%8E/g, "")
-    ?.replace(/%A7/g, "")
+  let cleanedName = name.split("/")[5]; // Get the song name part
+  toRemove.forEach(substring => {
+    cleanedName = cleanedName.replace(new RegExp(substring, "gi"), ''); // Replace each substring with an empty string
+  });
+  return cleanedName.trim(); // Trim any leading or trailing spaces
 };
 
 const CardDetails = ({ name }) => {
