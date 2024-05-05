26e33434fc33ff6646f83b10559214e120ab6e19
SoltaneKarim <karimsoltane92@gmail.com>
2024-05-05 04:51:40 +0100

    update css for mobile

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,9 +10,6 @@ function Footer() {
                 <li className="social-icon__item"><a className="social-icon__link" target="_blank" href="https://www.instagram.com/karim_ben_soltane/">
                     <img className="sm-logo" src="https://i.pinimg.com/564x/98/7a/43/987a435a0b0c3b3868ac0eca06e91965.jpg" alt="Instagram" />
                 </a></li>
-                <li className="social-icon__item"><a className="social-icon__link" target="_blank" href="https://github.com/SoltaneKarim">
-                    <img className="sm-logo" src="https://i.pinimg.com/564x/66/71/3a/66713a96b9b21dffd3a85a5d748a3171.jpg" alt="GitHub" />
-                </a></li>
                 <li className="social-icon__item"><a className="social-icon__link" target="_blank" href="https://www.linkedin.com/in/karim-soltane-416085290/">
                     <img className="sm-logo" src="https://i.pinimg.com/564x/8a/15/b6/8a15b646a6856d20854c2ac22623d971.jpg" alt="LinkedIn" />
                 </a></li>
